ed3049a78b40a2fa6d6737ad8e85bae73ba496ac
Wesley Odhiambo <Wesley@MacBookJoin.local>
2026-01-10 23:35:05 +0300

    Resume Feedback Page

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -70,7 +70,7 @@ const Upload = () => {
         data.feedback = JSON.parse(feedbackText);
         await kv.set(`resume: ${uuid}`, JSON.stringify(data));
         setStatusText('Analysis complete, redirecting.. ');
-        console.log(data);
+        navigate(`/resume${uuid}`)
 
     }
 
